e4e345368f3bcd67249c1932d234c98a2d785d21
Dmitry Zrayev <dmitryzrayev@gmail.com>
2018-01-05 21:22:53 +0200

    little fix models

--- app/Post.php
+++ app/Post.php
@@ -6,10 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model {
     protected $table = 'posts';
-    protected $guarded = [];
     protected $fillable = ['title', 'body'];
-
-    public function user() {
-        return $this->belongsTo(User::class, 'user_id');
-    }
 }
--- app/User.php
+++ app/User.php
@@ -5,8 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
     use Notifiable;
 
     /**
@@ -15,7 +14,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     /**
@@ -24,11 +25,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
-    public function generateToken()
-    {
+    public function generateToken() {
         $this->api_token = str_random(60);
         $this->save();
 
